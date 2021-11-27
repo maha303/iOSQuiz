64541f97d36a057d173211545c808b213e81917e
Maha <Memo1992mm2010@hotmail.com>
2021-11-27 21:40:47 +0300

    Initial Commit

---
+++ iOSQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSQuiz
+//
+//  Created by Maha saad on 22/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
